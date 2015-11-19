262a83bd7f4ddd3b5f7ac9e11782e2d393af41f1
Alexa Schreffler <alexa.schreffler@gmail.com>
2015-11-19 14:13:10 -0600

    first 3 tests passed

--- app/arrays.js
+++ app/arrays.js
@@ -3,15 +3,21 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    return arr.indexOf(item);
   },
 
   sum : function(arr) {
-
+    return eval(arr.join('+'));
   },
 
   remove : function(arr, item) {
-
+    
+    for(var i = 0; i<arr.length; i++) {
+      if(arr[i] == item) {
+        arr.splice(i, 1);
+      }
+    }
+    return arr;
   },
 
   removeWithoutCopy : function(arr, item) {
