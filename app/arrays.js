ca791ba8c9414f68c32e5c44fe0c7fcab68bc4c9
Alexa Schreffler <alexa.schreffler@gmail.com>
2015-11-20 09:49:41 -0600

    stuck on array duplicates

--- app/arrays.js
+++ app/arrays.js
@@ -11,7 +11,6 @@ exports.arraysAnswers = {
   },
 
   remove : function(arr, item) {
-    
     for(var i = 0; i<arr.length; i++) {
       if(arr[i] == item) {
         arr.splice(i, 1);
@@ -21,39 +20,66 @@ exports.arraysAnswers = {
   },
 
   removeWithoutCopy : function(arr, item) {
-
+    for(var i = arr.length-1; i>0; i--){
+       var v = arr[i];
+       for(var j = 1; j <arguments.length; j++){
+          if(v == arguments[j]){
+             arr.splice(i,1);
+          }
+       }
+    }
+    return arr;
   },
 
   append : function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate : function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend : function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail : function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat : function(arr1, arr2) {
-
+    var newArr = arr1.concat(arr2);
+    return newArr;
   },
 
   insert : function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count : function(arr, item) {
-
+    return arr.filter(function(arrItem){
+      return arrItem === item;
+    }).length;
   },
 
   duplicates : function(arr) {
-
+    var i,
+      len=arr.length,
+      out=[],
+      obj={};
+ 
+    for (i=0;i<len;i++) {
+      obj[arr[i]]=0;
+    }
+    for (i in obj) {
+      out.push(i);
+    }
+    return out;
   },
 
   square : function(arr) {
